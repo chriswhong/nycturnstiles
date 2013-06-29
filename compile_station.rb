d0e7c0bf1e3af984964d79529f67dbbf92cc63d6
Neal Shyam <neal@adstruc.com>
2013-06-29 14:45:06 -0400

    write out compile_station data
    this data still produces some erroneous data (huge negative swings on
    the graph/out of bound counts), plus it needs to be scrubbed for non
    whole-hour audit events (i.e. 4:37:00)
    
    would love some help with that!

--- compile_station.rb
+++ compile_station.rb
@@ -43,29 +43,32 @@ readfile = CSV.open $tfile, "r"
 
 # loop through date array and check hash for all elements that match current date. 
 # then sum entries/exits and assign them to the new hash called (table2) containing [date][entries][exits].
+# oh yeah, and write the whole thing to file.
 
-$count = 0
-table2 = Hash.new(0)
-$dates.each do |d|
+writefile = File.open "sum_"+$tfile,"a"
+	$count = 0
+	table2 = Hash.new(0)
+	$dates.each do |d|
 		
-		# now search hash (table) for all elements matching date (d)
-		# and then compile info into new hash.	[date, sum of entries across scp codes, sum of exits across scp codes]
+			# now search hash (table) for all elements matching date (d)
+			# and then compile info into new hash.	[date, sum of entries across scp codes, sum of exits across scp codes]
 		
-		table2[[$count,"date"]] = d
+			table2[[$count,"date"]] = d
 		
-		k = table.select{ |k,v| v == d}
-		ent = 0
-		ex = 0
-		k.each do |b|
-			ent += table[[b[0][0],"entries"]].to_i
- 			ex += table[[b[0][0],"exits"]].to_i
-		end
+			k = table.select{ |k,v| v == d}
+			ent = 0
+			ex = 0
+			k.each do |b|
+				ent += table[[b[0][0],"entries"]].to_i
+				ex += table[[b[0][0],"exits"]].to_i
+			end
 		
-		table2[[$count,"entries"]] = ent
-		table2[[$count,"exits"]] = ex
+			table2[[$count,"entries"]] = ent
+			table2[[$count,"exits"]] = ex
 		
-		#output check
-		puts table2[[$count,"date"]] + ", " + table2[[$count,"entries"]].to_s + ", "+ table2[[$count,"exits"]].to_s
+			#output check
+			puts table2[[$count,"date"]] + ", " + table2[[$count,"entries"]].to_s + ", "+ table2[[$count,"exits"]].to_s
+			writefile.puts table2[[$count,"date"]] + ", " + table2[[$count,"entries"]].to_s + ", "+ table2[[$count,"exits"]].to_s
 
-		$count += 1			
-end
+			$count += 1			
+	end
